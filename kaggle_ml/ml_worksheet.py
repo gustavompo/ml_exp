0d5aa9d9ea9acba7bd44cade16c7b3158539fec8
Gustavo Maloste <gustavomaloste@gmail.com>
2018-08-18 21:19:50 -0300

    learning

--- kaggle_ml/ml_worksheet.py
+++ kaggle_ml/ml_worksheet.py
@@ -16,3 +16,37 @@ iowa_model.fit(X, y)
 
 print("First in-sample predictions:", iowa_model.predict(X.head()))
 print("Actual target values for those homes:", y.head().tolist())
+
+from sklearn.model_selection import train_test_split
+
+# split data into training and validation data, for both features and target
+# The split is based on a random number generator. Supplying a numeric value to
+# the random_state argument guarantees we get the same split every time we
+# run this script.
+train_X, val_X, train_y, val_y = train_test_split(X, y, random_state = 1)
+
+from sklearn.metrics import mean_absolute_error
+val_mae = mean_absolute_error(val_predictions, val_y)
+
+# uncomment following line to see the validation_mae
+print(val_mae)
+step_4.check()
+
+
+
+def get_mae(max_leaf_nodes, train_X, val_X, train_y, val_y):
+    model = DecisionTreeRegressor(max_leaf_nodes=max_leaf_nodes, random_state=0)
+    model.fit(train_X, train_y)
+    preds_val = model.predict(val_X)
+    mae = mean_absolute_error(val_y, preds_val)
+    return(mae)
+    
+candidate_max_leaf_nodes = [5, 25, 50, 100, 250, 500]
+# Write loop to find the ideal tree size from candidate_max_leaf_nodes
+results = list((get_mae(a,train_X, val_X, train_y, val_y), a) for a in candidate_max_leaf_nodes)
+for (res, nodes) in results:
+    print("Mae %d\t\t%d" %(nodes, res))
+# Store the best value of max_leaf_nodes (it will be either 5, 25, 50, 100, 250 or 500)
+best_tree_size = 100
+
+step_1.check()
